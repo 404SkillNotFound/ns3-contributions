4b2de856a02bcaa26c1aff1488e71d15f5fa8169
404SkillNotFound <shivang.upadhyay1@gmail.com>
2026-02-21 03:31:57 +0530

    wifi: Replace C-style casts with reinterpret_cast

--- src/wifi/model/ssid.cc
+++ src/wifi/model/ssid.cc
@@ -68,7 +68,7 @@ Ssid::PeekString() const
 {
     // It is safe to return a pointer to the buffer because it is
     // guaranteed to be zero-terminated.
-    return (char*)m_ssid;
+    return reinterpret_cast<const char*>(m_ssid);
 }
 
 WifiInformationElementId
