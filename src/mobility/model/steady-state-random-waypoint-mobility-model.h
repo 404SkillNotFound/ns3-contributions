1f5f41030906e0d1e2c1bf6ecc450207f619ccf6
404SkillNotFound <shivang.upadhyay1@gmail.com>
2026-04-30 17:38:48 +0530

    mobility: Replace C-style casts with C++ casts

--- src/mobility/model/steady-state-random-waypoint-mobility-model.h
+++ src/mobility/model/steady-state-random-waypoint-mobility-model.h
@@ -86,7 +86,7 @@ class SteadyStateRandomWaypointMobilityModel : public MobilityModel
     Vector DoGetPosition() const override;
     void DoSetPosition(const Vector& position) override;
     Vector DoGetVelocity() const override;
-    int64_t DoAssignStreams(int64_t) override;
+    int64_t DoAssignStreamsstatic_cast<int64_t> override;
 
     ConstantVelocityHelper m_helper;            //!< helper for velocity computations
     double m_maxSpeed;                          //!< maximum speed value (m/s)
